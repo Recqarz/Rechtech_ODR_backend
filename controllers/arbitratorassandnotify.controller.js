f790a914464e4596e770f7db229f9ecba2139b26
Vikram Kumar <vikram.choudhary@recqarz.com>
2024-11-27 19:33:54 +0530

    Added the api for assign arbitrator in bulk

--- controllers/arbitratorassandnotify.controller.js
+++ controllers/arbitratorassandnotify.controller.js
@@ -1,6 +1,8 @@
 const { CASES } = require("../module/cases/case.model");
 const { USER } = require("../module/users/user.model");
-const { notificationtToall } = require("../services/notificationtoall");
+const {
+  notificationToall,
+} = require("../services/notificationtoall");
 
 const appointArbitratorandNotify = async (req, res) => {
   const { id, arbitratorName, arbitratorId, arbitratorEmail } = req.body;
@@ -14,7 +16,7 @@ const appointArbitratorandNotify = async (req, res) => {
     const user = await USER.findOne({ emailId: arbitratorEmail });
     let noOfAssignCase = parseInt(user.noOfAssignCase) + 1;
     await USER.findByIdAndUpdate(user._id, { noOfAssignCase }, { new: true });
-    await notificationtToall(cases);
+    await notificationToall(cases);
     return res.status(200).json({
       message: "Arbitrator Appointed and Notification sent successfully",
       updatedCases,
@@ -25,4 +27,33 @@ const appointArbitratorandNotify = async (req, res) => {
   }
 };
 
-module.exports = { appointArbitratorandNotify };
+const appointArbitratorandNotifyBulk = async (req, res) => {
+  const { data, arbitratorName, arbitratorId, arbitratorEmail } = req.body;
+  let count = 0;
+  try {
+    for (const caseData of data) {
+      const cases = await CASES.findById(caseData);
+      if (!isArbitratorAssigned) {
+        cases.arbitratorName = arbitratorName;
+        cases.arbitratorId = arbitratorId;
+        cases.arbitratorEmail = arbitratorEmail;
+        cases.isArbitratorAssigned = true;
+        await cases.save();
+        notificationToall(cases);
+        count++;
+      }
+    }
+    const user = await USER.findOne({ emailId: arbitratorEmail });
+    let noOfAssignCase = parseInt(user.noOfAssignCase) + count;
+    await USER.findByIdAndUpdate(user._id, { noOfAssignCase }, { new: true });
+    return res.status(200).json({
+      message: "Arbitrator Appointed and Notification sent successfully",
+      updatedCases,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { appointArbitratorandNotify, appointArbitratorandNotifyBulk };

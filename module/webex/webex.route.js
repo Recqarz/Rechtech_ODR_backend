391f8080afb87d98eb17a2b2b6cc71c30fc00000
Vikram Kumar <vikram.choudhary@recqarz.com>
2024-12-05 14:48:35 +0530

    Added the api for chart data,recent meetings, recent meetings with full details

--- module/webex/webex.route.js
+++ module/webex/webex.route.js
@@ -1,3 +1,4 @@
+const { recentMeeting, fullMeetingDataWithCaseDetails } = require("./recentwebex.controller");
 const {
   initializeToken,
   createMeeting,
@@ -10,6 +11,10 @@ const webexRouter = require("express").Router();
 webexRouter.post("/initialize-token", initializeToken);
 
 webexRouter.post("/create-meeting", ensureValidToken, createMeeting);
+
+webexRouter.get("/recent-meetings", recentMeeting);
+
+webexRouter.get("/recent-fullMeetingDataWithCaseDetails", fullMeetingDataWithCaseDetails);
 
 webexRouter.post(
   "/create-meeting-bulk",

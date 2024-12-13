bd51c0ad25e4a275b01cfe862b99142f5033642e
Vikram Kumar <vikram.choudhary@recqarz.com>
2024-12-13 18:34:58 +0530

    Added the api for single users data meetings and recent meetings

--- module/webex/webex.route.js
+++ module/webex/webex.route.js
@@ -1,4 +1,4 @@
-const { recentMeeting, fullMeetingDataWithCaseDetails, allMeetigs } = require("./recentwebex.controller");
+const { recentMeeting, fullMeetingDataWithCaseDetails, allMeetigs, recentMeetingArbitrator, fullMeetingDataWithCaseDetailsArbitrator, allMeetigsArbitrator } = require("./recentwebex.controller");
 const {
   initializeToken,
   createMeeting,
@@ -14,14 +14,20 @@ webexRouter.post("/create-meeting", ensureValidToken, createMeeting);
 
 webexRouter.get("/recent-meetings", recentMeeting);
 
+webexRouter.get("/recent-meetings/arbitrator", recentMeetingArbitrator);
+
 webexRouter.get("/recent-fullMeetingDataWithCaseDetails", fullMeetingDataWithCaseDetails);
 
+webexRouter.get("/recent-fullMeetingDataWithCaseDetails/arbitrator", fullMeetingDataWithCaseDetailsArbitrator);
+
 webexRouter.get("/all-meetings", allMeetigs);
+
+webexRouter.get("/all-meetings/arbitrator", allMeetigsArbitrator);
 
 webexRouter.post(
   "/create-meeting-bulk",
   ensureValidToken,
-  createMeetingforBulk
+  createMeetingforBulk,
 );
 
 module.exports = webexRouter;

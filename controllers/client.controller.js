f489d8a54f729f3535c86c860b08030aab67a815
sumansauravnov <suman.saurav@recqarz.com>
2025-01-07 14:47:24 +0530

    added the pagination

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,9 +1,25 @@
 const { USER } = require("../module/users/user.model");
 
+// All Client
 const allClient = async (req, res) => {
   try {
-    let client = await USER.find({ role: "client" }).sort({ _id: -1 });
-    return res.status(200).json({ user: client });
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page
+    const skip = (page - 1) * limit;
+
+    let client = await USER.find({ role: "client" })
+      .sort({ _id: -1 })
+      .skip(skip)
+      .limit(limit);
+    const totalCases = await USER.countDocuments({ role: "client" });
+    return res
+      .status(200)
+      .json({
+        user: client,
+        currentPage: page,
+        totalPages: Math.ceil(totalCases / limit),
+        totalCases,
+      });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal Server Error" });
